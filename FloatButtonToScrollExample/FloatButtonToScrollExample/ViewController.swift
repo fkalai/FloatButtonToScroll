b31ac778dce7baedfac945100bcba8f31a5601c4
Fotis Kalaitzidis <f.kalaitzidis@stoiximan.gr>
2020-09-15 22:26:18 +0300

    Added FloatButtonToScroll Example project

---
+++ FloatButtonToScrollExample/FloatButtonToScrollExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FloatButtonToScrollExample
+//
+//  Created by Fotios Kalaitzidis on 15/9/20.
+//  Copyright © 2020 Fotis Kalaitzidis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
